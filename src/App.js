0bc778988829dd095052341feff269d337629b41
Your Name <sasindusubodhaka@gmail.com>
2021-06-25 01:53:52 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import './components/assets/css/style.css';
 function App() {
   return (
     <div className="App">
-            <Router>
+            <Router basename={window.location.pathname || ''}>
                 <Navbar />
                 <Switch>
                     <Route path="/" exact component={Home}></Route>
